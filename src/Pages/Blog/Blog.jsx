3e4a39c5bfd214942497867e136ab64eba5fecdc
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-02 23:11:15 +0600

    added Footer Section

---
+++ src/Pages/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div className="my-container">
+      <h3>Blog are coming soon</h3>
+    </div>
+  );
+};
+
+export default Blog;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "base-100": "#FFFFFF",
-        },
-      },
-    ],
+  theme: {
+    extend: {},
   },
   plugins: [require("daisyui")],
 };
